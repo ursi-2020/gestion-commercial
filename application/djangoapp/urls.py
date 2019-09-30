e16e6cbff2e82bd3a4596d4408908ab916a153e0
Nicolas Cendrier <nicolas.cendrier@epita.fr>
2019-09-30 08:40:59 +0200

    scheduled call added

--- application/djangoapp/urls.py
+++ application/djangoapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('schedule/get-products', views.schedule_get_products_from_catalogue, name='schedule-get-product'),
     path('get-products', views.get_product_fom_catalogue, name='get-products'),
     path('display-products', views.display_products, name='display-products')
 ]
--- application/djangoapp/views.py
+++ application/djangoapp/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from apipkg import api_manager as api
 from application.djangoapp.models import *
-from .forms import UserForm, ArticleForm
-from .models import User
+from datetime import datetime, timedelta
 from django.views.decorators.csrf import csrf_exempt
 import json
+import requests
 
 
 def index(request):
@@ -14,16 +15,47 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+@csrf_exempt
 def get_product_fom_catalogue(request):
     Product.objects.all().delete()
-    data = api.send_request('catalogue-produit', 'catalogueproduit/api/data')
+    data = api.send_request('catalogue-produit', 'api/data')
     products = json.loads(data)['produits']
     for product in products:
         new_product = Product(**product)
         new_product.save()
-    return redirect(display_products)
+    print(str(len(products)) + " products were saved")
+    if request.method == "GET":
+        return redirect(display_products)
+    else:
+        return HttpResponse()
 
 
 def display_products(request):
     produits = Product.objects.all().order_by("familleProduit")
     return render(request, 'info_catalogue_produits.html', {"produits": produits})
+
+
+def schedule_get_products_from_catalogue(request):
+    clock_time = api.send_request('scheduler', 'clock/time')
+    time = datetime.strptime(clock_time, '"%d/%m/%Y-%H:%M:%S"')
+    time = time + timedelta(seconds=80)
+    time_str = time.strftime('%d/%m/%Y-%H:%M:%S')
+    body = {
+        "target_url": 'get-products',
+        "target_app": 'gestion-commerciale',
+        "time": time_str,
+        "recurrence": "minute",
+        "data": '{}',
+        "source_app": "gestion-commerciale",
+        "name": "test"
+    }
+    r = schedule_task(body)
+    return render(request, 'index.html', {})
+
+
+def schedule_task(body):
+    headers = {'Host': 'scheduler'}
+    r = requests.post(api.api_services_url + 'schedule/add', headers=headers, json=body)
+    print(r.status_code)
+    print(r.text)
+    return r.text
